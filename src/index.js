befc55498254d0cecf5d04eb80419ba6b891fe03
punSavitri <punshakshi@gmail.com>
2024-03-04 23:52:00 +0000

    added javascript funcationality by calling AI API

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 //alert("Testing!!!!");
-//This function generate the original poem based on the user information provided on input filed 
-function generatePoem(event) {
-  event.preventDefault();
-
+//this function show the response from the API call and embeded on HTML 
+function showPoem(response) {
+  console.log(response.data.answer);
+  console.log("Generating poem...");
   //adding typewriter plugin to animate text
   new Typewriter("#poem", {
-    strings: `Demain, dès l’aube, à l’heure où blanchit la campagne, Je partirai.
-  Vois - tu, je sais que tu m’attends.`,
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: ""
   })
+}
+//This function generate the original poem based on the user information provided on input filed 
+function generatePoem(event) {
+  event.preventDefault();
+  let userInstructionInput = document.querySelector("#user-instruction")
+  let apiKey = "4c3a38t82d64bfo4330f17ff02bfbd97";
+  let prompt = `User instruction:Generate a french poem based on ${userInstructionInput.value}`;
+  let context = "You are expert to generate romantic poem.please provide a poem in four line using in basic HTML format";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  //make a call to the API
+  axios.get(apiUrl).then(showPoem);
+  console.log("generating poem....");
+
+
 
 }
 //assigning variables to the form element and adding event listener
