d35d1a6c46005bd2025b4335389b76983b94c2e1
punSavitri <punshakshi@gmail.com>
2024-03-06 10:22:58 +0000

    added AI API integration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 //alert("Testing!!!!");
 //this function show the response from the API call and embeded on HTML 
 function showPoem(response) {
-  console.log(response.data.answer);
-  console.log("Generating poem...");
+  //console.log(response.data.answer);
   //adding typewriter plugin to animate text
   new Typewriter("#poem", {
     strings: response.data.answer,
@@ -16,13 +15,17 @@ function generatePoem(event) {
   event.preventDefault();
   let userInstructionInput = document.querySelector("#user-instruction")
   let apiKey = "4c3a38t82d64bfo4330f17ff02bfbd97";
-  let prompt = `User instruction:Generate a french poem based on ${userInstructionInput.value}`;
-  let context = "You are expert to generate romantic poem.please provide a poem in four line using in basic HTML format";
+  let context = "You are a romantic poem expert and love to write short poem. Your mission is to generate a 4 lines poem in basic HTML and separate line with a <br/>.Do not include a title of the poem.Sign the poem with 'SheCodes AI' inside a <strong> element at the bottom of the poem. Make sure to follow the user instruction.";
+  //generating the poem based on whatever the user typed in the input field 
+  let prompt = `User instruction:Generate a french poem about ${userInstructionInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  console.log("Generating poem...")
+  console.log(`prompt:${prompt}`);
+  console.log(`context:${context}`);
 
   //make a call to the API
   axios.get(apiUrl).then(showPoem);
-  console.log("generating poem....");
+
 
 
 
